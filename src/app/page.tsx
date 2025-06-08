75aa55c6cbe36bced14150d85548096e4e205239
Abid Famasya <abid.famasya@gmail.com>
2025-06-08 12:32:24 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,64 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="flex flex-col w-full max-w-4xl mx-auto my-12 px-8">
+      <div className="flex flex-row items-start gap-4">
+        <div>
+          <Image
+            src="/automagic-logo.png"
+            alt="Automagic Systems"
+            width={100}
+            height={100}
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div>
+          <h1 className="font-sora-sans font-medium text-2xl">Automagic Systems</h1>
+          <p className="mt-2">Human-first automation for a post-boring world</p>
+        </div>
+      </div>
+
+      <div className="mt-2 space-y-1">
+        <p>
+          Since the dawn of AI, one thing has become clear: humans should do less of the dull stuff and stay in charge.
+        </p>
+        <p>
+          At Automagic Systems, we build tools that automate the mundane, while keeping people in control.
+        </p>
+
+        <p className="font-sora-sans font-medium">
+          Because AI is here to serve, not to rule.
+        </p>
+      </div>
+
+      <div className="mt-8">
+        <h2 className="font-sora-sans font-medium text-2xl">Products</h2>
+        <Link href="https://zenfin.app?utm_source=automagic-landing&utm_medium=referral&utm_campaign=automagic-landing" target="_blank" className="space-y-2 bg-gradient-to-b from-white/10 to-white/0 p-4 rounded-lg border-white/20 border border-solid hover:border-white/40 transition-colors hover:bg-white/5 flex flex-col md:flex-row items-center gap-2 md:gap-8">
+          <div className="h-12 md:h-12 w-36 relative">
+            <Image src="/zenfin-white.png" alt="Zenfin" layout="fill" objectFit="contain" />
+          </div>
+          <div>
+            <h4 className="font-sora-sans font-bold">Zenfin</h4>
+            <p>Turns WhatsApp chats into personal money tracking and insights. No spreadsheets, no dashboards, no stress. Just chat like a human, we’ll handle the boring bits.</p>
+          </div>
+        </Link>
+      </div>
+
+      <div className="mt-8">
+        <h2 className="font-sora-sans font-medium text-2xl">Our believes</h2>
+        <ol className="space-y-1 list-decimal list-inside">
+          <li>Every system should have a brain.</li>
+          <li>Humans should be the boss, not the AI.</li>
+          <li>Good automation feels like magic.</li>
+          <li>AI isn't the future, it’s here late.</li>
+        </ol>
+      </div>
+
+      <div className="mt-8">
+        <h2 className="font-sora-sans font-medium text-2xl">Contact</h2>
+        <p>automagic[at]abidf.com</p>
+      </div>
     </div>
   );
 }
